d8529f41538002f89403f5b11ea3291f77806c26
Ingo Wolf <me@ingowolf.au>
2025-06-06 17:10:31 +1000

    add basic menubar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,36 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Resizable from '$lib/components/ui/resizable/index.js';
+	import * as Menubar from '$lib/components/ui/menubar/index.js';
+
+	let direction = <'horizontal' | 'vertical'>$state('horizontal');
 </script>
 
-<Resizable.PaneGroup class="h-screen w-screen" direction="horizontal">
-	<Resizable.Pane class="h-full w-full">One</Resizable.Pane>
+<Resizable.PaneGroup class="h-screen w-screen" {direction}>
+	<Resizable.Pane class="h-full w-full">
+		<div class="flex h-full w-full flex-col p-2">
+			<Menubar.Root class="h-fit">
+				<Menubar.Menu>
+					<Menubar.Trigger>File</Menubar.Trigger>
+					<Menubar.Content>
+						<Menubar.Item>Save</Menubar.Item>
+						<Menubar.Item>Open</Menubar.Item>
+					</Menubar.Content>
+				</Menubar.Menu>
+				<Menubar.Menu>
+					<Menubar.Trigger>View</Menubar.Trigger>
+					<Menubar.Content>
+						<Menubar.Item
+							onclick={() => (direction = direction === 'horizontal' ? 'vertical' : 'horizontal')}
+						>
+							Switch to {direction === 'horizontal' ? 'vertical' : 'horizontal'} split
+						</Menubar.Item>
+					</Menubar.Content>
+				</Menubar.Menu>
+				<Button variant="ghost" size="icon"></Button>
+			</Menubar.Root>
+		</div>
+	</Resizable.Pane>
 	<Resizable.Handle />
 	<Resizable.Pane class="h-full w-full">Output will be here</Resizable.Pane>
 </Resizable.PaneGroup>
