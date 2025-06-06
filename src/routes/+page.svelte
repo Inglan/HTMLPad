394180ac74192bb16756f0a7ab809f39d33b8102
Ingo Wolf <me@ingowolf.au>
2025-06-06 16:56:46 +1000

    add base structure

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import * as Resizable from '$lib/components/ui/resizable/index.js';
+</script>
+
+<Resizable.PaneGroup class="h-screen w-screen" direction="horizontal">
+	<Resizable.Pane class="h-full w-full">One</Resizable.Pane>
+	<Resizable.Handle />
+	<Resizable.Pane class="h-full w-full">Output will be here</Resizable.Pane>
+</Resizable.PaneGroup>
